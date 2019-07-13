f504bb2db8b7a8fe27b1a4e8f5a0644b3b4b017b
Wageesh Arya <32549605+WageeshArya@users.noreply.github.com>
2019-07-13 19:44:31 +0530

    These were for revision of basic DOM manipulation
    Only addition feature added yet. Need to add local storage feature as well as deletion feature

--- app.js
+++ app.js
@@ -1,69 +1,29 @@
-// book constructor
-function Book(title, author, isbn){
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
-}
+document.querySelector('form').addEventListener('submit',createBook)
 
-// UI constructor
-function UI(){}
-
-UI.prototype.showAlert = function(alert,message){
-
-    const form = document.getElementById('book-form');
-    const container = document.querySelector('.container');
-    const shownMsgDiv = document.createElement('div');
-    container.insertBefore(shownMsgDiv,form);
-    shownMsgDiv.innerText = `${message}`;
-    if(alert===0){
-        shownMsgDiv.classList.add('error');
-        setTimeout(function(){
-            shownMsgDiv.classList.remove('success');
-        },3000);
+function createBook(e){
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const isbn = document.getElementById('isbn');
+    if(title.value === '' || author.value === '' || isbn.value ===''){
+        showError(0);
     }
-    else
-    {
-        shownMsgDiv.classList.add('success');
-        setTimeout(function(){
-            shownMsgDiv.classList.remove('success');
-        },3000);
+    else{
+        tr = document.createElement('tr');
+        tr.innerHTML = `<td>${title.value}</td>
+                        <td>${author.value}</td>
+                        <td>${isbn.value}</td>
+                        <td><a href="#">X</a></td>`;
+        document.querySelector('tbody').appendChild(tr);
+        e.preventDefault();
     }
-    
-}
-
-UI.prototype.addBookToList = function(book){
-    const tbody = document.querySelector('.tbody');
-
-    const row = document.createElement('tr');
-
-    row.innerHTML=`<td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.isbn}</td>
-    <td><a href="#" class="delete">X</a></td>`;
-    tbody.appendChild(row);
-
-    document.getElementById('title').value='';
-    document.getElementById('author').value='';
-    document.getElementById('ISBN').value='';
-}
-
-//Event Listeners
-document.getElementById('book-form').addEventListener('submit',function(e){
+    title.value='';
+    author.value='';
+    isbn.value='';
     e.preventDefault();
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const isbn = document.getElementById('ISBN').value;
-    const ui = new UI();
-    if(title === '' || author === '' || isbn === '' )
-    {
-        ui.showAlert(0,'Please fill all the fields');
-    }
-    const book = new Book(title, author, isbn);
-    
-    ui.addBookToList(book);
-});
+}
 
-document.querySelector('table').addEventListener('onClick',function(e){
-    console.log(e.target);
-    e.preventDefault();
-});
+function showError(error){
+    if(error === 0){
+        console.log('Insufficient data');
+    }
+}
