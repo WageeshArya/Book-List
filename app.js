d6ed3be76cfbfa61558b5aae11d09a78bc9b67f7
Wageesh Arya <32549605+WageeshArya@users.noreply.github.com>
2019-07-14 00:47:37 +0530

    Only deletion from local storage left

--- app.js
+++ app.js
@@ -1,69 +1,90 @@
-// book constructor
-function Book(title, author, isbn){
-    this.title = title;
-    this.author = author;
-    this.isbn = isbn;
+document.querySelector('form').addEventListener('submit',createBook);
+document.addEventListener('DOMContentLoaded',loadUpCollection);
+
+function loadUpCollection(){
+    const books = JSON.parse(localStorage.getItem('collection'));
+    books.forEach(function(book){
+        loadUI(book);
+    })
 }
 
-// UI constructor
-function UI(){}
+function loadUI(book){
+    tr = document.createElement('tr');
+    tr.innerHTML = `<td>${book.title}</td>
+                        <td>${book.author}</td>
+                        <td>${book.isbn}</td>
+                        <td><a href="#">X</a></td>`;
+        document.querySelector('tbody').appendChild(tr);
+}
 
-UI.prototype.showAlert = function(alert,message){
+function createBook(e){
+    const title = document.getElementById('title');
+    const author = document.getElementById('author');
+    const isbn = document.getElementById('isbn');
+    if(title.value === '' || author.value === '' || isbn.value ===''){
+        showMessage(0);
+    }
+    else{
+        tr = document.createElement('tr');
+        tr.innerHTML = `<td>${title.value}</td>
+                        <td>${author.value}</td>
+                        <td>${isbn.value}</td>
+                        <td><a href="#">X</a></td>`;
+        document.querySelector('tbody').appendChild(tr);
+        e.preventDefault();
+    }
+    const book = {
+            title : title.value,
+            author : author.value,
+            isbn : isbn.value
+        }
 
-    const form = document.getElementById('book-form');
-    const container = document.querySelector('.container');
-    const shownMsgDiv = document.createElement('div');
-    container.insertBefore(shownMsgDiv,form);
-    shownMsgDiv.innerText = `${message}`;
-    if(alert===0){
-        shownMsgDiv.classList.add('error');
+    if(localStorage.getItem('collection') === null){
+        collection = [];
+    }
+    else{
+        collection = JSON.parse(localStorage.getItem('collection'));
+    }
+    console.log(collection);
+    collection.push(book);
+    localStorage.setItem('collection',JSON.stringify(collection));
+
+    title.value='';
+    author.value='';
+    isbn.value='';
+    e.preventDefault();
+}
+
+document.querySelector('table').addEventListener('click',deleteBook);
+
+function deleteBook(e){
+    if(e.target.innerText === 'X'){
+        const del = e.target;
+        del.parentElement.parentElement.remove();
+        showMessage(1);
+        console.log(del.parentElement.parentElement);
+    }
+}
+
+function showMessage(err){
+
+    const shownError = document.createElement('div');
+    document.querySelector('.container').insertBefore(shownError,document.querySelector('form'));
+
+    if(err === 0){
+        shownError.innerText = "Please fill out all the fields";
+        shownError.classList.add('error');
+        document.querySelector('.container').insertBefore(shownError,document.querySelector('form'));
         setTimeout(function(){
-            shownMsgDiv.classList.remove('success');
+            document.querySelector('.error').remove();
         },3000);
     }
-    else
-    {
-        shownMsgDiv.classList.add('success');
+    else if (err === 1){ 
+        shownError.innerText = "Successfully Deleted";
+        shownError.classList.add('success');
+        document.querySelector('.container').insertBefore(shownError,document.querySelector('form'));
         setTimeout(function(){
-            shownMsgDiv.classList.remove('success');
+            document.querySelector('.success').remove();
         },3000);
     }
-    
 }
-
-UI.prototype.addBookToList = function(book){
-    const tbody = document.querySelector('.tbody');
-
-    const row = document.createElement('tr');
-
-    row.innerHTML=`<td>${book.title}</td>
-    <td>${book.author}</td>
-    <td>${book.isbn}</td>
-    <td><a href="#" class="delete">X</a></td>`;
-    tbody.appendChild(row);
-
-    document.getElementById('title').value='';
-    document.getElementById('author').value='';
-    document.getElementById('ISBN').value='';
-}
-
-//Event Listeners
-document.getElementById('book-form').addEventListener('submit',function(e){
-    e.preventDefault();
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const isbn = document.getElementById('ISBN').value;
-    const ui = new UI();
-    if(title === '' || author === '' || isbn === '' )
-    {
-        ui.showAlert(0,'Please fill all the fields');
-    }
-    const book = new Book(title, author, isbn);
-    
-    ui.addBookToList(book);
-});
-
-document.querySelector('table').addEventListener('onClick',function(e){
-    console.log(e.target);
-    e.preventDefault();
-});
